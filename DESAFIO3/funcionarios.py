8360292af2c0974a3b0f5cb79a2147a8d76f9ab3
itzmarcos <marcozfuwa@gmail.com>
2026-09-11 14:43:23 -0300

    desafio 3 atualizado

--- DESAFIO3/funcionarios.py
+++ DESAFIO3/funcionarios.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+import locale
 
 
 class Funcionario(ABC):
-    def __init__(self, nome):
+    def __init__(self, nome, salario):
         self.nome = nome
-        self._salario = None
+        self._salario = salario
 
     @property
     def salario(self):
@@ -14,17 +15,36 @@ class Funcionario(ABC):
     def salario(self, valor):
         self._salario = valor
 
+    @property
+    def fsalario(self):
+        locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+        return locale.currency(self._salario, grouping=True, symbol=True)
+
     def calcular_bonus():
         pass
 
+    
+
 class Gerente(Funcionario):
 #BONUS 15%
-    pass
+    def __str__(self):
+        return f'{self.nome} ganha {self.fsalario} e por ser Gerente o bonus é de R$ {self.calcular_bonus():.2f}'
+
+    def calcular_bonus(self):
+        return self.salario * 0.15
 
 class Designer(Funcionario):
 #BONUS 8%
-    pass
+    def __str__(self):
+        return f'{self.nome} ganha {self.fsalario} e por ser Designer o bonus é de R$ {self.calcular_bonus():.2f}'
+
+    def calcular_bonus(self):
+        return self.salario * 0.08
 
 class Desenvolvedor(Funcionario):
 #BONUS 10%
-    pass 
+    def __str__(self):
+        return f'{self.nome} ganha {self.fsalario} e por ser Desenvolvedr o bonus é de R$ {self.calcular_bonus():.2f}'
+
+    def calcular_bonus(self):
+        return self.salario * 0.10
--- DESAFIO3/main.py
+++ DESAFIO3/main.py
@@ -1,7 +1,8 @@
 from funcionarios import *
 
 def main():
-    pass
+    f = Designer("Pedro", 2500)
+    print(f)
 
 if __name__ == "__main__":
     main()

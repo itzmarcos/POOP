ef5ce87edbee77a7bb44457c25d785c8d1133976
itzmarcos <marcozfuwa@gmail.com>
2026-09-18 14:38:28 -0300

    add test06

---
+++ TEST/test06/diariosecreto.py
@@ -0,0 +1,11 @@
+class Diario:
+    def __init__(self, senhamestra = 'CeV!@'):
+        self.__segredos = []
+        self.__senha = senhamestra
+
+
+    def escrever(self):
+        pass
+
+    def ler(self):
+        pass

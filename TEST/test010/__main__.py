d2047f2137265ab611d1c2acd40f5ebce836d522
itzmarcos <marcozfuwa@gmail.com>
2026-09-23 17:21:07 -0300

    test10 incompleto

--- TEST/test010/__main__.py
+++ TEST/test010/__main__.py
@@ -4,8 +4,8 @@ from banco import ContaBancaria
 
 def main():
     c = ContaBancaria(20, 'Marcelo', 1000)
-
-    print(c)
+    c.sacar = 500
+    print(c.sacar)
 
 
 if __name__ == '__main__':
--- TEST/test010/banco.py
+++ TEST/test010/banco.py
@@ -2,16 +2,18 @@ from hashlib import sha256
 
 
 class ContaBancaria():
-    def __init__(self, id: int, nome, saldo = 0):
+    def __init__(self, id: int, nome:str, saldo:float = 0, chave:str = None):
         self._id = id
         self._titular = nome
-        self.__saldo = 0
+        self.__saldo = saldo
         self.__hash = None
-        #@nome
+        if chave is None:
+            chave = self.pede_senha()
+        self.__hash = sha256(chave.encode()).hexdigest()
+        print(f'Conta {self._id} criada com sucesso!\nSaldo atual de R${self.__saldo:.2f}')
 
-    @property
-    def hash(self):
-        return self.__hash
+    def pede_senha(self):
+        pass
 
     @property
     def saldo(self):
@@ -20,6 +22,16 @@ class ContaBancaria():
     @property
     def nome(self):
         return self.__titular
+
+    @nome.setter
+    def nome(self, novonome:str = None):
+        chave = self.pede_senha()
+
+        if self.validar_senha(chave):
+            if len(novonome) >= 5:
+                self._titular = novonome
+        else:
+            print('Senha nao confere')
 
     @saldo.setter
     def saldo(self, saldo):
@@ -32,25 +44,33 @@ class ContaBancaria():
     def validar_senha(self,chave):
         usuario = sha256(chave.encode('utf-8')).hexdigest()
         if usuario == self.__hash:
-            print('Senha conferi')
+            return True
         else:
-            print('Senha não conferi')
+            return False
+
+    def __str__(self):
+        return f'A conta {self._id} de {self._titular} tem R${self.__saldo:,.2f} de saldo'
 
 
     def pede_senha():
         pass
 
         
-    def sacar(self, valor, chave):
-        try:
-            saque = input(valor)
-            return saque
-        except:
-            print('Tente novamente')
+    def sacar(self, valor:float, chave:str = None):
+        valor = abs(valor)
+        if chave is None:
+            chave = self.pede_senha()
+
+        if self.validar_senha(chave):
+            if valor > self.__saldo:
+                print(f'Saque NEGADO!')
+            else:
+                self.__saldo -= valor
+                print(f'Saque de R${valor:,.2f} autorizado na conta {self._id}')
+        else:
+            print('Senha nao confere')
 
     def depositar(self, valor):
-        try:
-            deposita = input(valor)
-            return deposita
-        except:
-            print('Tente novamente')
+        valor = abs(valor)
+        self.__saldo += valor
+        print(f'Deposito de R${valor:,.2f} autorizado!')
